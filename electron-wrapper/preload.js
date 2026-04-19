076a60ffc2eeb2fe01976cdc2fcf059c14369633
grahamonero <grahamonero@users.noreply.github.com>
2026-04-19 07:54:29 -0700

    Update README.md file

--- electron-wrapper/preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron')
-
-contextBridge.exposeInMainWorld('api', {
-  recheckPear: () => ipcRenderer.invoke('recheck-pear'),
-  launchPear: () => ipcRenderer.invoke('launch-pear'),
-  openInstallGuide: () => ipcRenderer.invoke('open-install-guide'),
-  copyInstallCmd: () => ipcRenderer.invoke('copy-install-cmd'),
-  quit: () => ipcRenderer.invoke('quit')
-})
